913c70ea0e7afdd6a2f17f6773a49072962f36b8
Caleb Weeks <cweeks@imsdirect.com>
2022-12-15 18:16:12 -0500

    Add package info

--- mix.exs
+++ mix.exs
@@ -7,22 +7,36 @@ defmodule Exoteric.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: [
+        main: "readme",
+        extras: ["README.md"],
+        api_reference: false
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
     ]
   end
+
+  defp description() do
+    "A random collection of questionably useful Elixir tools."
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{}
+    ]
+  end
+
 end
